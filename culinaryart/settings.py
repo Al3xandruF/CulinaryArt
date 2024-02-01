050e467c647007020466b4b0febf2a1a36eedd01
Al3xandruF <al3x.florea@gmail.com>
2024-02-01 16:09:45 +0000

    Add recipe model/ update settings

--- culinaryart/settings.py
+++ culinaryart/settings.py
@@ -80,6 +80,7 @@ INSTALLED_APPS = [
     "comments",
     "likes",
     "followers",
+    "recipes",
 ]
 SITE_ID = 1
 MIDDLEWARE = [
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """
+    Recipe model, related to 'owner'
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    preparation = models.IntegerField()
+    cook = models.IntegerField()
+    difficulty = models.CharField(max_length=50)
+    servings = models.IntegerField()
+    calories = models.IntegerField()
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
