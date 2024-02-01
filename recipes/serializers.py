277c63550f25f95ea62a0a467ef52e8b2f22f081
Al3xandruF <al3x.florea@gmail.com>
2024-02-01 16:45:46 +0000

    Add recipe serializer

---
+++ recipes/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    is_owner = serializers.SerializerMethodField()
+    title = serializers.CharField(max_length=255)
+    description = serializers.TextField()
+    preparation = serializers.IntegerField()
+    cook = serializers.IntegerField()
+    difficulty = serializers.CharField(max_length=50)
+    servings = serializers.IntegerField()
+    calories = serializers.IntegerField()
+
+    def get_is_owner(self, obj):
+        request = self.context["request"]
+        return request.user == obj.owner
+
+    class Meta:
+        model = Recipe
+        fields = (
+            "owner",
+            "created_at",
+            "updated_at",
+            "title",
+            "description",
+            "preparation",
+            "cook",
+            "difficulty",
+            "servings",
+            "calories",
+        )
