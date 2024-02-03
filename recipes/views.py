17ba2ab46fb9f975b9415b628a846994c45dee16
Al3xandruF <al3x.florea@gmail.com>
2024-02-03 15:02:01 +0000

    Update recipes serializers/create views/add urls

---
+++ recipes/views.py
@@ -0,0 +1,54 @@
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from culinaryart.permissions import IsOwnerOrReadOnly
+from .models import Recipe
+from .serializers import RecipeSerializer
+
+
+class RecipeDetailList(generics.ListCreateAPIView):
+    """
+    API endpoint for listing and creating new added Recipe instances.
+    """
+
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Recipe.objects.annotate(
+        likes_count=Count("likes", distinct=True),
+        comments_count=Count("recipecomment", distinct=True),
+    ).order_by("-created_at")
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        "owner__followed__owner__profile",
+        "likes__owner__profile",
+        "owner__profile",
+    ]
+    search_fields = [
+        "owner__username",
+        "title",
+    ]
+    ordering_fields = [
+        "likes_count",
+        "comments_count",
+        "likes__created_at",
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Post.objects.annotate(
+        likes_count=Count("likes", distinct=True),
+        comments_count=Count("recipecomment", distinct=True),
+    ).order_by("-created_at")
