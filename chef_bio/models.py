70d4451d03f1562e466eb10bcdaf13c1b2da49c0
Al3xandruF <al3x.florea@gmail.com>
2024-02-04 13:55:20 +0000

    Create chef_bio app and add model

---
+++ chef_bio/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ChefBio(models.Model):
+    """
+    Chef Profile user bio details.
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    chef_title = models.CharField(max_length=255, blank=True)
+    place_of_birth = models.CharField(max_length=255, blank=True)
+    cooking_school = models.CharField(max_length=255, blank=True)
+    speciality_dish = models.TextField(blank=True)
+    achievements = models.TextField(blank=True)
+    web_address = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.owner}'s Chef Bio"
