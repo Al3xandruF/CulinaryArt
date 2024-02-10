3bca35bb1d1dcb6bc78802c2e2110ed4abb8123b
Al3xandruF <al3x.florea@gmail.com>
2024-02-10 18:36:17 +0000

    Update likes views

--- likes/views.py
+++ likes/views.py
@@ -29,3 +29,76 @@ class LikeDetail(generics.RetrieveDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
+
+
+class RecipeLikeList(generics.ListCreateAPIView):
+    """
+    API endpoint for listing all recipe likes and create a
+    new recipe like instance.
+    """
+
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    serializer_class = RecipeLikeSerializer
+    queryset = RecipeLike.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class RecipeLikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    API endpoint to retrieve or delete a specific recipe like instance.
+    """
+
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    serializer_class = RecipeLikeSerializer
+    queryset = RecipeLike.objects.all()
+
+
+class RecipeCommentLikeList(generics.ListCreateAPIView):
+    """
+    API endpoint to list all recipe comment likes and create a
+    new recipe comment like instance.
+    """
+
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    serializer_class = RecipeCommentLikeSerializer
+    queryset = RecipeCommentLike.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class RecipeCommentLikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    API endpoint to retrieve or delete a specific recipe
+    comment like instance.
+    """
+
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    serializer_class = RecipeCommentLikeSerializer
+    queryset = RecipeCommentLike.objects.all()
+
+
+class RecipeSavedList(generics.ListCreateAPIView):
+    """
+    API endpoint to list all saved recipes and create a
+    new saved recipe instance.
+    """
+
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    serializer_class = RecipeSavedSerializer
+    queryset = RecipeSaved.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class RecipeSavedDetail(generics.RetrieveDestroyAPIView):
+    """
+    API endpoint to retrieve or delete a specific saved recipe instance.
+    """
+
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    serializer_class = RecipeSavedSerializer
+    queryset = RecipeSaved.objects.all()
