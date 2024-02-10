d14fe5bb025edbef67268a366d3c920f43f9521e
Al3xandruF <al3x.florea@gmail.com>
2024-02-10 18:42:43 +0000

    Update likes.urls with all relevant paths

--- likes/urls.py
+++ likes/urls.py
@@ -4,4 +4,10 @@ from likes import views
 urlpatterns = [
     path("likes/", views.LikeList.as_view()),
     path("likes/<int:pk>/", views.LikeDetail.as_view()),
+    path("recipe-likes/", views.RecipeLikeList.as_view()),
+    path("recipe-likes/<int:pk>/", views.RecipeLikeDetail.as_view()),
+    path("recipe-comment-likes/", views.RecipeCommentLikeList.as_view()),
+    path("recipe-comment-likes/<int:pk>/", views.RecipeCommentLikeDetail.as_view()),
+    path("recipe-saved/", views.RecipeSavedList.as_view()),
+    path("recipe-saved/<int:pk>/", views.RecipeSavedDetail.as_view()),
 ]
