c9690ee3ce963eaea2f902e9836d415642735e25
Al3xandruF <al3x.florea@gmail.com>
2024-02-17 12:54:39 +0000

    Update comments urls for typo

--- comments/urls.py
+++ comments/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("recipe-comments/", views.RecipeCommentList.as_view()),
     path("recipe-comments/<int:pk>/", views.RecipeCommentDetail.as_view()),
     path(
-        "recipe-comment-replies/<int:parent_comment_id>/",
+        "recipe-comment-replies/<int:main_comment_id>/",
         views.RecipeCommentReplyList.as_view(),
     ),
     path(
