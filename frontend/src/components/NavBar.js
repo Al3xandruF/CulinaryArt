f14b1d4dd816f74677a122b43b85a1d16e5a71b2
Al3xandruF <al3x.florea@gmail.com>
2024-02-21 15:18:07 +0000

    Add logo to project

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="50" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
