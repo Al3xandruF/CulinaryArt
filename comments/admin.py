1cee307cd06cce0b836260cecd5cceedf4c6e40a
Al3xandruF <al3x.florea@gmail.com>
2024-02-21 11:39:26 +0000

    Add/register apps for admin page

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import RecipeComment, RecipeCommentReply
 
-# Register your models here.
+admin.site.register(RecipeComment)
+admin.site.register(RecipeCommentReply)
