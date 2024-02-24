c8eff655d4a09ff553cf009ef319ac00a0acce07
Al3xandruF <al3x.florea@gmail.com>
2024-02-24 15:19:06 +0000

    Add auth forms and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route exact path="/chefs" render={() => <h1>Chefs</h1>} />
           <Route exact path="/recipes" render={() => <h1>Recipes</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <h2>Page Not Found!</h2>} />
         </Switch>
       </Container>
