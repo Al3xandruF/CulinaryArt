55dd98de951feb6fe0fabd03cf8223cefe8b25f9
Al3xandruF <al3x.florea@gmail.com>
2024-03-17 14:08:03 +0000

    Create SignIn form/update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home</h1>} />
           <Route exact path="/chefs" render={() => <h1>Chefs</h1>} />
           <Route exact path="/recipes" render={() => <h1>Recipes</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <h2>Page Not Found!</h2>} />
         </Switch>
